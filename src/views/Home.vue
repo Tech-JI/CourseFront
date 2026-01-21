5646dd623fcd7e7ca5fa4587fea2b76dfa050021
alexis <liang_sr@sjtu.edu.cn>
2026-01-21 15:51:44 +0800

    refactor: mv api call of landing to src/composables/useLanding.js

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -141,12 +141,13 @@ import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
 import { MagnifyingGlassIcon } from "@heroicons/vue/24/outline";
 import { useAuth } from "../composables/useAuth";
-import { apiFetch } from "../utils/api";
+import { useLanding } from "../composables/useLanding";
 
 const router = useRouter();
 const reviewCount = ref(0);
 const { isAuthenticated } = useAuth();
 const searchQuery = ref("");
+const { fetchLanding } = useLanding();
 
 onMounted(async () => {
   await fetchLandingData();
@@ -154,11 +155,7 @@ onMounted(async () => {
 
 const fetchLandingData = async () => {
   try {
-    const response = await apiFetch("/api/landing/");
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
+    const data = await fetchLanding();
     reviewCount.value = data.review_count;
   } catch (error) {
     console.error("Error fetching landing data:", error);
